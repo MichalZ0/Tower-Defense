58a5b6b8376add25f506b62578bdf8ca15ceb7d2
MichalZ0 <mzdzieblo007@interia.pl>
2024-11-07 14:27:49 +0100

    Volume slider

--- Components/SliderModule.py
+++ Components/SliderModule.py
@@ -3,6 +3,9 @@ import pygame
 class Slider:
     def __init__(self, screen, size, pos):
         self.screen = screen
+        self.pressed = 0
+        self.pos = (0,0)
+        self.volume = 0
 
         self.sliderRect = pygame.Rect(pos[0], pos[1], size[0], size[1] )
 
@@ -17,6 +20,31 @@ class Slider:
 
     def draw(self):
         self.screen.blit(self.sliderSurface, (self.sliderRect.x, self.sliderRect.y))
+
+
+    def clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            self.pressed = 1
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.pressed = 0
+        
+        self.sliderLowestPos = self.sliderRect.x + self.sliderHead.width/2
+        self.sliderHighestPos = (self.sliderRect.x + self.sliderRect.width) - self.sliderHead.width/2
+
+        if event.type == pygame.MOUSEMOTION and self.pressed == 1:
+            if (self.sliderRect.collidepoint(event.pos)):
+                self.sliderSurface = pygame.Surface(self.sliderRect.size)
+                pygame.draw.rect(self.sliderSurface, "red", (0,0, self.sliderRect.width, self.sliderRect.height), border_radius=10)
+                
+                if (event.pos[0] > self.sliderLowestPos and event.pos[0] < self.sliderHighestPos):
+                    self.sliderHeadDrawPos = event.pos
+
+                self.sliderHead = pygame.draw.circle(self.sliderSurface, "white", (self.sliderHeadDrawPos[0]-self.sliderRect.x, self.sliderRect.height/2), self.sliderRect.height/2)
+                self.volume = (self.sliderHeadDrawPos[0] * 100) / (self.sliderRect.width + self.sliderRect.x- (self.sliderHead.width/2))
+                print(self.volume)
+
+             
+            
 
 
 
