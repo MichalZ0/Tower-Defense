4ef8414be45d93f45f523376d1f2e5d121378cd5
MichalZ0 <mzdzieblo007@interia.pl>
2024-12-08 17:50:36 +0100

    Panel wyboru wiezy

--- Components/SidePanel.py
+++ Components/SidePanel.py
@@ -1,8 +1,10 @@
 import pygame
+from Components.ButtonModule import *
+from defenders import *
 
 
 class SidePanel:
-    def __init__(self, screen, width, height, sf, health:int, money:int, width_size=150):
+    def __init__(self, screen, width, height, sf, health:int, money:int, width_size=150): 
         self.width_size = width_size
         self.screen = screen
         self.width = width
@@ -29,6 +31,49 @@ class SidePanel:
         self.panel_image = pygame.transform.scale(self.panel_image,
                                                   (self.width_size * self.sf, self.height))
 
+
+        self.itemPath = os.path.join(os.getcwd(), "assets", "towers") 
+        self.itemSize = (40, 40)
+        
+        self.towerCount = 10
+        self.towerColumnCount = 3
+        self.towerButtons = []
+        self.itemGap = 5 
+
+        self.towerPanelRect = pygame.Rect(
+                self.width - width_size,
+                300,
+                self.towerColumnCount * self.itemSize[0] + (self.towerColumnCount + 1) * self.itemGap, 
+                int(self.towerCount / self.towerColumnCount) * self.itemSize[0] + (self.towerColumnCount + 1) * self.itemGap, 
+                )
+
+        self.towerPanelRect.x += int(width_size / 2 - self.towerPanelRect.width / 2)
+
+
+        self.towerPanel = pygame.Surface((self.towerPanelRect.width, self.towerPanelRect.height))
+        self.towerPanel.fill("brown")
+
+
+        for i in range(0, int(self.towerCount/self.towerColumnCount)):
+            for j in range(0, self.towerColumnCount):
+                self.item = Button(self.towerPanel, 
+                                   self.itemSize, 
+                                   (j * self.itemSize[0] + ((j+1) * self.itemGap), i * self.itemSize[1] + ((i +1) * self.itemGap)),
+                                   'white', "Cannon", image='Cannon')
+                self.cannonItemImage = pygame.image.load(os.path.join(self.itemPath, "Cannon", "Cannon1.png"))
+
+                self.item.onClick(self.placeItem)
+                self.towerButtons.append(self.item)
+
+
+
+        self.newTower = None
+        self.towerImage = None
+        self.towerClicked = False
+        self.drawTowerPosition = [101,101]
+
+
+
     def draw(self, wave_num, max_waves, wave_running, won:bool, lost:bool):
         if not won and not lost:
             self.screen.blit(self.panel_image, (self.width - self.width_size * self.sf, 0))
@@ -50,6 +95,7 @@ class SidePanel:
             self.screen.blit(self.coin, (572 * self.sf, -55 * self.sf))
             self.screen.blit(self.pixel_font.render(str(self.money), True, 'white'), (722 * self.sf, 57 * self.sf))
             self.screen.blit(self.pixel_font.render(str(self.health), True, 'white'), (722 * self.sf, 109 * self.sf))
+
         # Dodawanie tekstu na przycisk
             if not wave_running:
                 text_surface = self.button_font.render(self.button_text, True, (255, 255, 255))
@@ -58,11 +104,88 @@ class SidePanel:
             text_rect = text_surface.get_rect(center=self.button_rect.center)
             self.screen.blit(text_surface, text_rect)
 
-    def handle_event(self, event):
+            self.drawTowerPanel()
+
+
+
+    def placeItem(self):
+        return True
+
+
+    def drawTowerPanel(self):
+        for towerIcon in self.towerButtons:
+            towerIcon.draw()
+            
+
+        self.screen.blit(self.towerPanel, self.towerPanelRect)
+
+        if self.towerImage != None:
+            self.screen.blit(self.towerImage, self.drawTowerPosition)
+
+
+    def handle_event(self, event, sprites):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.button_rect.collidepoint(event.pos):
-                self.button_pressed = True
-                return True  # Przycisnięcie wykryte
-        elif event.type == pygame.MOUSEBUTTONUP:
-            self.button_pressed = False
-        return False
+                    self.button_pressed = True
+                    return True  # Przycisnięcie wykryte
+            elif event.type == pygame.MOUSEBUTTONUP:
+                self.button_pressed = False
+            return False
+
+    def handleTowerSelection(self, event, sprites):
+        towerMap = {'Cannon': Cannon,
+                    'Temple': Cannon, 
+                    'Archer': Cannon,
+                    'Wizard': Cannon}
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if (event.pos[0] >= self.towerPanelRect.x and 
+                event.pos[1] >= self.towerPanelRect.y):
+            
+                for towerButton in self.towerButtons:
+                    clickRect = towerButton.getRect()
+                    clickRect.x += self.width - self.width_size
+                    clickRect.y += self.towerPanelRect.y
+
+                    self.towerClicked = towerButton.clicked(event, clickRect, checkFullRect=True)
+                    
+
+                    if (self.towerClicked == True):
+                        self.newTower = towerMap[towerButton.getText()](self.drawTowerPosition,
+                                                                        self.itemPath,
+                                                                        100,
+                                                                        100,
+                                                                        1)
+                        break 
+
+        if (event.type == pygame.MOUSEMOTION and self.towerClicked == True):
+
+
+            if (event.pos[0] > 0 and
+                event.pos[0] - self.newTower.getRect().width/2 > 0 and 
+                event.pos[0] <= self.towerPanelRect.x): 
+
+                sprites.add(self.newTower)
+                
+                self.drawTowerPosition = [event.pos[0], event.pos[1]]
+                self.newTower.setPosition(event.pos)
+
+                self.newTower.showRadius()
+            
+
+            elif (event.pos[0] > self.towerPanelRect.x):
+                sprites.remove(self.newTower)
+
+            if (event.buttons[0] == 0):
+                self.towerClicked = False
+                self.newTower.hideRadius()
+                
+                return True
+
+
+    
+    def getTower(self):
+        if (self.newTower != None): 
+            return self.newTower
+        
+
