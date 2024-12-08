159ad5159f4df310ae8ac6ec2c6b2a9d80bf0bf4
MichalZ0 <mzdzieblo007@interia.pl>
2024-12-08 19:24:36 +0100

    Dodaj obrazki do przyciskow

--- Components/ButtonModule.py
+++ Components/ButtonModule.py
@@ -15,7 +15,7 @@ def isHovered(event):
 
 class Button:
     buttons = []
-    def __init__(self, screen, size, position, color, text, padding=30, contentPosition=None, borderRadius=0, hover=False, hoverBg = None):
+    def __init__(self, screen, size, position, color, text, padding=30, contentPosition=None, borderRadius=0, hover=False, hoverBg = None, image_path=None):
         self.screen = screen
         self.position = position
         self.color = color
@@ -24,11 +24,12 @@ class Button:
         self.size = size
         self.borderRadius = borderRadius
         self.buttonRect = pygame.Rect(self.position[0], self.position[1], self.size[0], self.size[1])
+        self.image_path = image_path
         self.clickFunction = lambda: print('') 
 
         self.font = pygame.font.Font("Fonts/OpenSans-Regular.ttf", 24)
         self.textObject = self.font.render(self.text, True, (255,255,255))
-        self.buttonSurface = pygame.Surface(( self.buttonRect.width, self.buttonRect.height ))
+        self.buttonSurface = pygame.Surface(( self.buttonRect.width, self.buttonRect.height ), pygame.SRCALPHA)
         self.hovered = 0
 
         if (contentPosition==None):
@@ -45,14 +46,20 @@ class Button:
         self.position = pos
 
     def draw(self):
-        self.buttonSurface.set_colorkey((0,0,0))
         self.buttonRect = pygame.draw.rect(self.buttonSurface, self.color, (0,0,self.buttonSurface.get_width(), self.buttonSurface.get_height()), 
                          width=0, border_radius=self.borderRadius)
 
         self.buttonRect.x = self.position[0]
         self.buttonRect.y = self.position[1]
 
-        self.buttonSurface.blit(self.textObject, self.contentPosition)
+        if (self.image_path == None): 
+            self.buttonSurface.blit(self.textObject, self.contentPosition)
+        else:
+            self.buttonImg = pygame.image.load(self.image_path)
+            # self.buttonImg.fill('blue')
+            self.buttonImg = pygame.transform.scale(self.buttonImg, self.size)
+            self.buttonSurface.blit(self.buttonImg, (0,0)) 
+
         self.screen.blit(self.buttonSurface, (self.position[0], self.position[1]))
 
 
@@ -80,7 +87,7 @@ class Button:
         return self.clickFunction
 
 
-    def clicked(self, event, rect=None):
+    def clicked(self, event, rect=None, checkFullRect=False):
         self.buttonMask = pygame.mask.from_surface(self.buttonSurface)
 
         if (rect != None):
@@ -91,9 +98,11 @@ class Button:
 
 
         if (self.buttonClickRect.collidepoint(event.pos)):
-            pos = (event.pos[0]-self.buttonClickRect.x,event.pos[1]-self.buttonClickRect.y)
-            if (self.buttonMask.get_at(pos) == 1):
-                return self.clickFunction()
+            if (checkFullRect == True):
+                pos = (event.pos[0]-self.buttonClickRect.x,event.pos[1]-self.buttonClickRect.y)
+                if (self.buttonMask.get_at(pos) == 1):
+                    return self.clickFunction()
+            return self.clickFunction()
 
     def hover(self):
         self.color = 'gray'
